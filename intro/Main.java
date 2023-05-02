e93a64e0a4a818003c35eb8197c42a442525304b
dlorenzo2000 <deanlorenzo@gmail.com>
2023-05-02 08:13:06 -0500

    Added interface.

--- intro/Main.java
+++ intro/Main.java
@@ -1,3 +1,19 @@
+interface Animal {
+    public void animalSound();
+
+    public void sleep();
+}
+
+class Dog implements Animal {
+    public void animalSound() {
+        System.out.println("Dogs bark.");
+    }
+
+    public void sleep() {
+        System.out.println("Zzz...");
+    }
+}
+
 public class Main {
     static int plusMethod(int x, int y) {
         return x + y;
@@ -12,5 +28,8 @@ public class Main {
         double myNum2 = plusMethod(4.3, 6.26);
         System.out.println("int: " + myNum1);
         System.out.println("double: " + myNum2);
+        Dog dog = new Dog();
+        dog.animalSound();
+        dog.sleep();
     }
 }
